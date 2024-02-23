31b9241c593a2031d0745a68d30541b686910996
THANH\21110 <thanh.161003@gmail.com>
2024-02-23 21:57:24 +0700

    first commit

--- app/src/main/java/com/example/week3/MenuActivity.java
+++ app/src/main/java/com/example/week3/MenuActivity.java
@@ -1,9 +1,12 @@
 package com.example.week3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,12 +14,32 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu_setting,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.menuSetting:
+//            //Lệnh
+//                break;
+//            case R.id.menuShare:
+//                break;
+//            case R.id.menuLogout:
+//                break;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+//    private void ShowPopupMenu(){
+//        PopupMenu popupMenu = new PopupMenu(this,
+//                );
+//        popupMenu.getMenuInflater().inflate(R.menu.menu_setting,popupMenu.getMenu());
+//        popupMenu.show();
+//    }
 }
